292b486654a4b1917b359802f0b0bafd61630fe6
yakuznecov <yakuznecov@mail.ru>
2021-11-02 22:20:15 +0300

    add listener

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Элементы формы
+
+const squareInput = document.querySelector('#square-input');
+const squareRange = document.querySelector('#square-range');
+
+// Связать range с текстовым полем, слушаем событие инпута
+
+squareRange.addEventListener('input', function () {
+    
+})
+
+
