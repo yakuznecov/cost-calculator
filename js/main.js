b0b0a9554d989009bd3126f472419c679425148b
yakuznecov <yakuznecov@mail.ru>
2021-11-03 23:30:25 +0300

    add price

--- js/main.js
+++ js/main.js
@@ -3,10 +3,18 @@
 const squareInput = document.querySelector('#square-input');
 const squareRange = document.querySelector('#square-range');
 
+const basePrice = 6000; // Базовая цена
+
 // Связать range с текстовым полем, слушаем событие инпута
 
 squareRange.addEventListener('input', function () {
-    
-})
+	squareInput.value = squareRange.value;
+});
 
+// Связать текстовое поле с ползунком
 
+squareInput.addEventListener('input', function () {
+	squareRange.value = squareInput.value;
+});
+
+const totalPrice = basePrice * parseInt(squareInput.value); // приводим строку из инпута к числу
